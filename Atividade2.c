51b7fe2a28ff9b147a699c8a3abb1a0d4ed20621
nti <nti@localhost.localdomain>
2017-01-26 23:06:21 -0300

    Alterando Exibição

--- Atividade2.c
+++ Atividade2.c
@@ -6,6 +6,7 @@ int main()
     int numeros[20][10];
     for(int i = 0; i < 20; i++) {
         for(int j = 0; j < 10; j++) {
+            numeros[i][j] = i+j;
             printf("%d ", numeros[i][j]);
         }
         printf("\n");
